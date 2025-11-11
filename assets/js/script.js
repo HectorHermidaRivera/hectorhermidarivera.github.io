430ed14245026477f945a32ab76ffbc9dce596ae
HéctorHermidaRivera <28502360+HectorHermidaRivera@users.noreply.github.com>
2025-11-11 10:46:43 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
 
 // COLLAPSIBLES
   document.querySelectorAll('.collapsible-link').forEach(link => {
@@ -28,4 +28,4 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-});
+//});
