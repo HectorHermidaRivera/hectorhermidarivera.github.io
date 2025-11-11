d9a201b4ef62167248f106f033be83d143522f55
HéctorHermidaRivera <28502360+HectorHermidaRivera@users.noreply.github.com>
2025-11-11 10:56:16 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,9 @@
 
 window.addEventListener('load', () => {
   if (window.location.hash) {
-    const el = document.querySelector(window.location.hash);
-    if (el) el.scrollIntoView({ behavior: 'smooth' });
+    setTimeout(() => {
+      const el = document.querySelector(window.location.hash);
+      if (el) el.scrollIntoView({ behavior: 'smooth' });
+    }, 250);
   }
 });
