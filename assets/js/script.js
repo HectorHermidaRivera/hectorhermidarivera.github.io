10976e02a159f667c8d9a41d44248a7c2e9914ca
HéctorHermidaRivera <28502360+HectorHermidaRivera@users.noreply.github.com>
2025-11-20 22:03:43 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,13 @@
 // COLLAPSIBLES
+
 document.querySelectorAll('.collapsible-link').forEach(link => {
   link.addEventListener('click', function (e) {
-    // 1. Find the closest container .value
     const container = link.closest('.value');
     if (!container) return;
 
-    // 2. Find the first collapsible-content inside this container
     const content = container.querySelector('.collapsible-content');
     if (!content) return;
 
-    // 3. Toggle maxHeight
     if (content.style.maxHeight && content.style.maxHeight !== '0px') {
       content.style.maxHeight = '0';
     } else {
@@ -19,17 +17,18 @@ document.querySelectorAll('.collapsible-link').forEach(link => {
 });
 
 // OPEN EXTERNAL LINKS IN NEW TAB
+
 document.querySelectorAll('a').forEach(link => {
   if (
-    !link.classList.contains('collapsible-link') && // exclude collapsible links
-    !link.classList.contains('same-tab') &&         // exclude same-tab links
-    !link.closest('.sidebar')                       // exclude sidebar links
+    !link.classList.contains('collapsible-link') && 
+    !link.classList.contains('same-tab') 
   ) {
     link.setAttribute('target', '_blank');
   }
 });
 
 // HASH JUMPS
+
 function scrollToHashInstantly() {
   if (!window.location.hash) return;
   const el = document.querySelector(window.location.hash);
